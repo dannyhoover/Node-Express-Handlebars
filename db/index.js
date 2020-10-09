7f64b2e0580117ad2dcd2dde7c3ea87b137d180c
dannyhoover <dofjrtd@gmail.com>
2020-10-08 18:43:22 -0700

    index, schema, seed, partials updates

--- db/index.js
+++ db/index.js
@@ -7,16 +7,12 @@ const connection = mysql.createConnection(process.env.JAWSDB_URL || {
   database : 'burger_db'
 });
 
-const query = function(query, values) {
-  console.log(Object.values(arguments));
-  return new Promise((resolve, reject) => {
-    connection.query(...Object.values(arguments), (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    })
-  })
-}
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-module.exports = {
-    connection
-}
+module.exports = connection;
