e1c0e2292d828a9c6d7a5f016e641aad1f735014
dannyhoover <dofjrtd@gmail.com>
2020-10-08 17:49:44 -0700

    Post tutoring progress
    Co-authored-by: Wesley Clements <wesley.clements@gmail.com>

---
+++ db/index.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
+  host     : 'localhost',
+  user     : 'root',
+  password : process.env.LOCAL_MYSQL_PASSWORD,
+  database : 'burger_db'
+});
+
+const query = function(query, values) {
+  console.log(Object.values(arguments));
+  return;
+  return new Promise((resolve, reject) => {
+    connection.query(...Object.values(arguments), (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    })
+  })
+}
+
+module.exports = {
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express = require("express");
+const bodyParse = require("body-parser");
+const serveStatic = require("serve-static");
+const exphbs  = require('express-handlebars');
+
+require("./db");
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+
+app.set('view engine', 'handlebars');
+
+//#region middleware
+app.use(bodyParse.urlencoded({extended: false}));
+app.use(bodyParse.json());
+
+app.use("/static", serveStatic("public"));
+//#endregion
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => {
+  console.log("App started: 🌎 => PORT:" + PORT);
+});
