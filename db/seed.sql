e4012b173b51e1bfa769cf5c53d3c384d4a558ae
dannyhoover <dofjrtd@gmail.com>
2020-10-08 20:34:40 -0700

    schema table update

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,4 @@
 INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers (name) VALUES ('The Mac Attack');
 INSERT INTO burgers (name, eaten) VALUES ('The Wilbur Burger', true);
-INSERT INTO burgers (name, eaten) VALUES ('McDouble', true);
-INSERT INTO burgers (name, eaten) VALUES ('Whopper', true);
 INSERT INTO burgers (name) VALUES ('Plain Hanburger');
