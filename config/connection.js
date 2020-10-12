625bc0cc0b1444024d03f7236cdc61a56b58ad0d
dannyhoover <dofjrtd@gmail.com>
2020-10-12 10:56:38 -0700

    trying to get heroku to run

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,27 @@
-const mysql = require("mysql");
+var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Ashley&Flock08",
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'Ashley&Flock08',
+    database: 'burger_db'
+  })
+};
 
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+var mysql = require('mysql');
 module.exports = connection;
