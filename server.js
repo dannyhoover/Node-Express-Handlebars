156edc512aa9e5089ea4efaea4e76b04a70643b7
dannyhoover <dofjrtd@gmail.com>
2020-10-12 13:09:27 -0700

    design updates: image shows, layout update

--- server.js
+++ server.js
@@ -35,10 +35,13 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+app.use(express.static('public/assets')); 
+
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -51,8 +54,10 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
